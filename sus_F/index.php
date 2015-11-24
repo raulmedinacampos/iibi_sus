881f57196ebf36157a6fad0f6e8ea62c5c3304e0
Raúl Medina <raulmedinacampos@yahoo.com.mx>
2015-11-24 16:40:36 -0600

    SUS Controller
    Se separó el código de SUS en el archivo controllers/sus_controller.php

--- sus_F/index.php
+++ sus_F/index.php
@@ -64,24 +64,7 @@ Flight::route('/principal/', function() {
 });
 
 Flight::route('/sus/', function() {
-	session_start();
-	
-	$empleado = (isset($_SESSION['idEmpleado'])) ? seleccionar('*','empleado',"idEmpleado=".$_SESSION['idEmpleado']) : "";
-	$responsable = (isset($_SESSION['idUAutoriza'])) ? seleccionar('*','empleado',"idEmpleado=".$_SESSION['idUAutoriza']) : "";
-	$area = (isset($_SESSION['idUAutoriza'])) ? seleccionar('area','cArea,puesto,empleado','cArea.idArea = puesto.idArea and puesto.estatus = 1 and puesto.idEmpleado=empleado.idEmpleado and empleado.idEmpleado = '.$_SESSION['idUAutoriza']) : "";
-	
-	
-	$nomUsuario = ($empleado) ? $empleado['gradoAcad']." ".$empleado['nombre']." ".$empleado['apellidoP']." ".$empleado['apellidoM'] : "";
-	$nomResponsable =  $responsable['gradoAcad']." ".$responsable['nombre']." ".$responsable['apellidoP']." ".$responsable['apellidoM'];
-	
-	$data = array(
-			'area' => $area,
-			'empleado' => $empleado,
-			'nomResponsable' => $nomResponsable,
-			'nomUsuario' => $nomUsuario
-	);
-	
-	Flight::render('servicios/sus', $data);
+	require_once 'controllers/sus_controller.php';
 });
 
 Flight::route('/guardaSUS/', function() {
