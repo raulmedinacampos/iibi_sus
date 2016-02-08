8b6981964059fa4d30baa4ed56bdc667c416698f
DafneBar <dafne.abad@gmail.com>
2016-02-08 15:05:25 -0600

    por json

--- sus_F/controllers/infoMensual_controller.php
+++ sus_F/controllers/infoMensual_controller.php
@@ -1,67 +1,65 @@
 <?php
 session_start();
  
-$columnas= "count(*)"
+$columnas= "count(*)";
 $tablas = "servicioSUS";
-
 
 $mes = $_POST["mes"];
 $anio = $_POST["anio"];
 
-// falta condición de fecha
 $condicion = "idTipoServicio like '2%' and month(fechaSolicitud)=".$mes." and year(fechaSolitud)=".$anio;
-$correspSol= seleccionar($columnas, $tablas, $condicion);
+$correspS= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "idTipoServicio like '2%' and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
-$correspReal= seleccionar($columnas, $tablas, $condicion);
+$correspR= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "idTipoServicio = '41' and month(fechaSolicitud)=".$mes." and year(fechaSolitud)=".$anio;
-$fotocSol= seleccionar($columnas, $tablas, $condicion);
+$fotocS= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "idTipoServicio = '41' and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
-$fotocReal= seleccionar($columnas, $tablas, $condicion);
+$fotocR= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "idTipoServicio ='42' and month(fechaSolicitud)=".$mes." and year(fechaSolicitud)=".$anio;
-$engarSol= seleccionar($columnas, $tablas, $condicion);
+$engarS= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "idTipoServicio = '42' and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
-$engarReal= seleccionar($columnas, $tablas, $condicion);
+$engarR= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "idTipoServicio between 32 and 34 and month(fechaSolicitud)=".$mes." and year(fechaSolicitud)=".$anio;
-$mtoEqSol= seleccionar($columnas, $tablas, $condicion);
+$mtoEqS= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "idTipoServicio between 32 and 34 and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
-$mtoEqReal= seleccionar($columnas, $tablas, $condicion);
+$mtoEqR= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "idTipoServicio like '6%' and month(fechaSolicitud)=".$mes." and year(fechaSolicitud)=".$anio;
-$mtoInmSol= seleccionar($columnas, $tablas, $condicion);
+$mtoInmS= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "idTipoServicio like '6%' and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
-$mtoInmReal= seleccionar($columnas, $tablas, $condicion);
+$mtoInmR= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "idTipoServicio = 31 and month(fechaSolicitud)=".$mes." and year(fechaSolicitud)=".$anio;
-$mtoVehiSol= seleccionar($columnas, $tablas, $condicion);
+$mtoVehiS= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "idTipoServicio = 31 and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
-$mtoVehiReal= seleccionar($columnas, $tablas, $condicion);
+$mtoVehiR= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "idTipoServicio like '5%' and month(fechaSolicitud)=".$mes." and year(fechaSolicitud)=".$anio;
-$transpSol= seleccionar($columnas, $tablas, $condicion);
+$transpS= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "idTipoServicio like '5%' and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
-$transpReal= seleccionar($columnas, $tablas, $condicion);
+$transpR= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "idTipoServicio = '11' and month(fechaSolicitud)=".$mes." and year(fechaSolicitud)=".$anio;
-$limpSol= seleccionar($columnas, $tablas, $condicion);
+$limpS= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "idTipoServicio = '11' and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
-$limpReal= seleccionar($columnas, $tablas, $condicion);
+$limpR= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "idTipoServicio = '71' and month(fechaSolicitud)=".$mes." and year(fechaSolicitud)=".$anio;
-$segSol= seleccionar($columnas, $tablas, $condicion);
+$segS= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "idTipoServicio = '71' and and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
-$segReal= seleccionar($columnas, $tablas, $condicion);
+$segR= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "month(fechaSolicitud)=".$mes." and year(fechaSolicitud)=".$anio;
 $totalS= seleccionar($columnas, $tablas, $condicion);
@@ -78,31 +76,31 @@ $sadm= seleccionar("concat(nombre,' ',apellidoP,' ',apellidoM) as nombre","puest
 /*Las variables que terminan con Sol son las solicitadas, las que terminan con Real son las realizadas*/
 
 $data = array(
-		'correspSol'  	=> $correspSol[0],
-		'fotocSol'	  	=> $fotocSol[0],
-		'engarSol'	  	=> $engarSol[0], 
-		'mtoEqSol'		=> $mtoEqSol[0],
-		'mtoInmSol'		=>  $mtoInmSol[0],
-		'mtoVehiSol'	=> $mtoVehiSol[0],
-		'transpSol'		=> $transpSol[0],
-		'limpSol'		=> $limpSol[0],
-		'segSol'		=> $segSol[0],
+		'correspS'  => $correspS[0],
+		'fotocS'	=> $fotocS[0],
+		'engarS'	=> $engarS[0], 
+		'mtoEqS'	=> $mtoEqS[0],
+		'mtoInmS'	=>  $mtoInmS[0],
+		'mtoVehiS'	=> $mtoVehiS[0],
+		'transpS'	=> $transpS[0],
+		'limpS'		=> $limpS[0],
+		'segS'		=> $segS[0],
 		
-		'correspReal' 	=> $correspReal[0],
-		'fotocReal'	  	=> $fotocReal[0],
-		'engarReal'	  	=> $engarReal[0],
-		'mtoEqReal'		=> $mtoEqReal[0],
-		'mtoInmReal'	=>  $mtoInmReal[0],
-		'mtoVehReal'	=> $mtoVehiReal[0],
-		'transpReal'	=> $transpReal[0],
-		'limpReal'		=> $limpReal[0],
-		'segReal'		=> $segReal[0],
+		'correspR' 	=> $correspR[0],
+		'fotocR'	=> $fotocR[0],
+		'engarR'	=> $engarR[0],
+		'mtoEqR'	=> $mtoEqR[0],
+		'mtoInmR'	=>  $mtoInmR[0],
+		'mtoVehR'	=> $mtoVehiR[0],
+		'transpR'	=> $transpR[0],
+		'limpR'		=> $limpR[0],
+		'segR'		=> $segR[0],
 		
-		'totalR' 		=> $totalR[0],
-		'totalS' 		=> $totalS[0],
-		'sg' 			=> $sg[0],
-		'sadm' 			=> $sadm[0],
+		'totalR' 	=> $totalR[0],
+		'totalS' 	=> $totalS[0],
+		'sg' 		=> $sg[0],
+		'sadm' 		=> $sadm[0],
 );
 
-
+echo json_encode($data);
 ?>
