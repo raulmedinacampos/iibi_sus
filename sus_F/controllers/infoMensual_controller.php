4f28c746cf6b431ae15db5545945c8254e377bcc
DafneBar <dafne.abad@gmail.com>
2016-02-08 12:14:38 -0600

    infoMensual completo

--- sus_F/controllers/infoMensual_controller.php
+++ sus_F/controllers/infoMensual_controller.php
@@ -9,41 +9,65 @@ $mes = $_POST["mes"];
 $anio = $_POST["anio"];
 
 // falta condición de fecha
-$condicion = "idTipoServicio like '2%' and month(fechaSolicitud)=".$mes." and year(fechaSolitud)=".$anio." and estatus<12";
+$condicion = "idTipoServicio like '2%' and month(fechaSolicitud)=".$mes." and year(fechaSolitud)=".$anio;
 $correspSol= seleccionar($columnas, $tablas, $condicion);
 
-$condicion = "idTipoServicio like '2%' and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio." and estatus>12";
+$condicion = "idTipoServicio like '2%' and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
 $correspReal= seleccionar($columnas, $tablas, $condicion);
 
-$condicion = "idTipoServicio = '41' and estatus<12";
+$condicion = "idTipoServicio = '41' and month(fechaSolicitud)=".$mes." and year(fechaSolitud)=".$anio;
 $fotocSol= seleccionar($columnas, $tablas, $condicion);
 
-$condicion = "idTipoServicio = '42' and estatus<12";
+$condicion = "idTipoServicio = '41' and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
+$fotocReal= seleccionar($columnas, $tablas, $condicion);
+
+$condicion = "idTipoServicio ='42' and month(fechaSolicitud)=".$mes." and year(fechaSolicitud)=".$anio;
 $engarSol= seleccionar($columnas, $tablas, $condicion);
 
-$condicion = "idTipoServicio = '33' and estatus<12";
+$condicion = "idTipoServicio = '42' and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
+$engarReal= seleccionar($columnas, $tablas, $condicion);
+
+$condicion = "idTipoServicio between 32 and 34 and month(fechaSolicitud)=".$mes." and year(fechaSolicitud)=".$anio;
 $mtoEqSol= seleccionar($columnas, $tablas, $condicion);
 
-$condicion = "idTipoServicio = '35' and estatus<12";
+$condicion = "idTipoServicio between 32 and 34 and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
+$mtoEqReal= seleccionar($columnas, $tablas, $condicion);
+
+$condicion = "idTipoServicio like '6%' and month(fechaSolicitud)=".$mes." and year(fechaSolicitud)=".$anio;
 $mtoInmSol= seleccionar($columnas, $tablas, $condicion);
 
-$condicion = "idTipoServicio = '31' and estatus<12";
+$condicion = "idTipoServicio like '6%' and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
+$mtoInmReal= seleccionar($columnas, $tablas, $condicion);
+
+$condicion = "idTipoServicio = 31 and month(fechaSolicitud)=".$mes." and year(fechaSolicitud)=".$anio;
 $mtoVehiSol= seleccionar($columnas, $tablas, $condicion);
 
-$condicion = "idTipoServicio like '5%' and estatus<12";
+$condicion = "idTipoServicio = 31 and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
+$mtoVehiReal= seleccionar($columnas, $tablas, $condicion);
+
+$condicion = "idTipoServicio like '5%' and month(fechaSolicitud)=".$mes." and year(fechaSolicitud)=".$anio;
 $transpSol= seleccionar($columnas, $tablas, $condicion);
 
-$condicion = "idTipoServicio = '11' and estatus<12";
+$condicion = "idTipoServicio like '5%' and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
+$transpReal= seleccionar($columnas, $tablas, $condicion);
+
+$condicion = "idTipoServicio = '11' and month(fechaSolicitud)=".$mes." and year(fechaSolicitud)=".$anio;
 $limpSol= seleccionar($columnas, $tablas, $condicion);
 
-$condicion = "idTipoServicio = '71' and estatus<12";
+$condicion = "idTipoServicio = '11' and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
+$limpReal= seleccionar($columnas, $tablas, $condicion);
+
+$condicion = "idTipoServicio = '71' and month(fechaSolicitud)=".$mes." and year(fechaSolicitud)=".$anio;
 $segSol= seleccionar($columnas, $tablas, $condicion);
 
-$condicion = "estatus<12";
-$totalR= seleccionar($columnas, $tablas, $condicion);
+$condicion = "idTipoServicio = '71' and and month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
+$segReal= seleccionar($columnas, $tablas, $condicion);
 
-$condicion = "1";
+$condicion = "month(fechaSolicitud)=".$mes." and year(fechaSolicitud)=".$anio;
 $totalS= seleccionar($columnas, $tablas, $condicion);
+
+$condicion = "month(fechaLiberacion)=".$mes." and year(fechaLiberacion)=".$anio;
+$totalR= seleccionar($columnas, $tablas, $condicion);
 
 $condicion = "empleado.idEmpleado = puesto.idEmpleado and puesto.puesto ='Jefe de área' and  puesto.idArea = 9 and puesto.estatus = 1";
 $sg= seleccionar("concat(nombre,' ',apellidoP,' ',apellidoM) as nombre","puesto, empleado", $condicion);
@@ -55,15 +79,25 @@ $sadm= seleccionar("concat(nombre,' ',apellidoP,' ',apellidoM) as nombre","puest
 
 $data = array(
 		'correspSol'  	=> $correspSol[0],
-		'correspReal' 	=> $correspReal[0],
 		'fotocSol'	  	=> $fotocSol[0],
 		'engarSol'	  	=> $engarSol[0], 
 		'mtoEqSol'		=> $mtoEqSol[0],
-		'mtoInm' 		=>  $mtoInmSol[0],
+		'mtoInmSol'		=>  $mtoInmSol[0],
 		'mtoVehiSol'	=> $mtoVehiSol[0],
 		'transpSol'		=> $transpSol[0],
 		'limpSol'		=> $limpSol[0],
 		'segSol'		=> $segSol[0],
+		
+		'correspReal' 	=> $correspReal[0],
+		'fotocReal'	  	=> $fotocReal[0],
+		'engarReal'	  	=> $engarReal[0],
+		'mtoEqReal'		=> $mtoEqReal[0],
+		'mtoInmReal'	=>  $mtoInmReal[0],
+		'mtoVehReal'	=> $mtoVehiReal[0],
+		'transpReal'	=> $transpReal[0],
+		'limpReal'		=> $limpReal[0],
+		'segReal'		=> $segReal[0],
+		
 		'totalR' 		=> $totalR[0],
 		'totalS' 		=> $totalS[0],
 		'sg' 			=> $sg[0],
