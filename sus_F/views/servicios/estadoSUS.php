a71d692cceb345fe8d4c4469bd8a38785f5e3ddb
Raúl Medina <raulmedinacampos@yahoo.com.mx>
2016-03-03 17:58:01 -0600

    Filtros de búsqueda

--- sus_F/views/servicios/estadoSUS.php
+++ sus_F/views/servicios/estadoSUS.php
@@ -1,12 +1,59 @@
+<link rel="stylesheet" type="text/css" href="css/bootstrap-datepicker.min.css" />
 <link href="css/servicios.css" rel="stylesheet" type="text/css" media="screen" />
 
+<script src="js/bootstrap-datepicker.min.js" type="text/javascript"></script>
+<script src="js/bootstrap-datepicker.es.min.js" type="text/javascript"></script>
 <script type="text/javascript" src="js/estado_solicitud.js"></script>
 
-<h3>Estado de solicitudes del mes de <?php echo $mes; ?></h3>
+<h3>Estado de solicitudes</h3>
 
-<div class="botones">
-	<button class="btn btn-sm btn-info">Consultar mes anterior</button>
-	<button class="btn btn-sm btn-info">Consultar mes siguiente</button>
+<div class="panel panel-primary">
+	<div class="panel-heading">Filtros de búsqueda</div>
+	<div class="panel-body">
+		<form method="post" id="formFiltro" name="formFiltro" class="form-horizontal" action="">
+			<div class="form-group">
+				<label class="col-sm-2 control-label">Fecha inicial</label>
+				<div class="col-sm-4">
+					<div class="input-group">
+						<input id="fechaInicial" name="fechaInicial" class="form-control datepicker" />
+						<span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
+					</div>
+				</div>
+				
+				<label class="col-sm-2 control-label">Fecha final</label>
+				<div class="col-sm-4">
+					<div class="input-group">
+						<input id="fechaFinal" name="fechaFinal" class="form-control datepicker" />
+						<span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
+					</div>
+				</div>
+			</div>
+			
+			<div class="form-group">
+				<label class="col-sm-2 control-label">Tipo de servicio</label>
+				<div class="col-sm-4">
+					<select id="tipoServicio" name="tipoServicio" class="form-control">
+						<option value="">Seleccione</option>
+					</select>
+				</div>
+				
+				<label class="col-sm-2 control-label">Estado</label>
+				<div class="col-sm-4">
+					<select id="estado" name="estado" class="form-control">
+						<option value="">Seleccione</option>
+					</select>
+				</div>
+			</div>
+			
+			<div class="form-group">
+				<div class="col-sm-12">
+					<div class="pull-right">
+						<button id="btnBuscar" name="btnBuscar" class="btn btn-primary">Buscar</button>
+					</div>
+				</div>
+			</div>
+		</form>
+	</div>
 </div>
 
 <?php
