edf24a680c7ee52bf6e623f7cc20096c5f9569e0
Raúl Medina <raulmedinacampos@yahoo.com.mx>
2016-03-04 17:41:30 -0600

    Validación mes en combobox

--- sus_F/js/informe_mensual.js
+++ sus_F/js/informe_mensual.js
@@ -3,10 +3,24 @@ function inicializar() {
 }
 
 function validar() {
+	jQuery.validator.addMethod("fechaValida", function(value, element, param) {
+		var anio = $(param).val();
+		var fecha = new Date();
+		var mesActual = fecha.getMonth()+1;
+		var anioActual = fecha.getFullYear();
+		
+		if ( anio <= anioActual ) {
+			return this.optional(element) || value <= mesActual;
+		} else {
+			return this.optional(element);
+		}
+	}, "Verifica que el mes seleccionado sea correcto");
+	
 	$("#formInforme").validate({
 		rules: {
 			mes: {
-				required: true
+				required: true,
+				fechaValida: "#anio"
 			},
 			anio: {
 				required: true
@@ -35,72 +49,65 @@ function cargarInfo() {
 	$("#btnCargar").click(function(e) {
 		e.preventDefault();
 		
-		if ( $("#formInforme").valid() ) {
-			var mes = $("#mes").val();
-			var anio = $("#anio").val();
-			
-			if ( mes && anio ) {
-				$.post(
-					'reportes/cargar-informe-mensual',
-					{'mes': mes, 'anio': anio},
-					function(data) {
-						var d = jQuery.parseJSON(data);
-						
-						$("#tdCorrespS").html(d.correspS);
-						$("#tdCorrespR").html(d.correspR);
-						$("#hCorrespS").val(d.correspS);
-						$("#hCorrespR").val(d.correspR);
-						
-						$("#tdFotocS").html(d.fotocS);
-						$("#tdFotocR").html(d.fotocR);
-						$("#hFotocS").val(d.fotocS);
-						$("#hFotocR").val(d.fotocR);
-						
-						$("#tdEngarS").html(d.engarS);
-						$("#tdEngarR").html(d.engarR);
-						$("#hEngarS").val(d.engarS);
-						$("#hEngarR").val(d.engarR);
-						
-						$("#tdMtoEqS").html(d.mtoEqS);
-						$("#tdMtoEqR").html(d.mtoEqR);
-						$("#hMtoEqS").val(d.mtoEqS);
-						$("#hMtoEqR").val(d.mtoEqR);
-						
-						$("#tdMtoInmS").html(d.mtoInmS);
-						$("#tdMtoInmR").html(d.mtoInmR);
-						$("#hMtoInmS").val(d.mtoInmS);
-						$("#hMtoInmR").val(d.mtoInmR);
-						
-						$("#tdMtoVehS").html(d.mtoVehiS);
-						$("#tdMtoVehR").html(d.mtoVehR);
-						$("#hMtoVehS").val(d.mtoVehiS);
-						$("#hMtoVehR").val(d.mtoVehR);
-						
-						$("#tdTranspS").html(d.transpS);
-						$("#tdTranspR").html(d.transpR);
-						$("#hTranspS").val(d.transpS);
-						$("#hTranspR").val(d.transpR);
-						
-						$("#tdLimpS").html(d.limpS);
-						$("#tdLimpR").html(d.limpR);
-						$("#hLimpS").val(d.limpS);
-						$("#hLimpR").val(d.limpR);
-						
-						$("#tdSegS").html(d.segS);
-						$("#tdSegR").html(d.segR);
-						$("#hSegS").val(d.segS);
-						$("#hSegR").val(d.segR);
-						
-						$("#tdTotalR").html(d.totalR);
-						$("#tdTotalS").html(d.totalS);
-						$("#hTotalR").val(d.totalR);
-						$("#hTotalS").val(d.totalS);
-						
-						$(".oculto").slideDown();
-					}
-				);
+		$.post(
+			'reportes/cargar-informe-mensual',
+			{},
+			function(data) {
+				var d = jQuery.parseJSON(data);
+				
+				$("#tdCorrespS").html(d.correspS);
+				$("#tdCorrespR").html(d.correspR);
+				$("#hCorrespS").val(d.correspS);
+				$("#hCorrespR").val(d.correspR);
+				
+				$("#tdFotocS").html(d.fotocS);
+				$("#tdFotocR").html(d.fotocR);
+				$("#hFotocS").val(d.fotocS);
+				$("#hFotocR").val(d.fotocR);
+				
+				$("#tdEngarS").html(d.engarS);
+				$("#tdEngarR").html(d.engarR);
+				$("#hEngarS").val(d.engarS);
+				$("#hEngarR").val(d.engarR);
+				
+				$("#tdMtoEqS").html(d.mtoEqS);
+				$("#tdMtoEqR").html(d.mtoEqR);
+				$("#hMtoEqS").val(d.mtoEqS);
+				$("#hMtoEqR").val(d.mtoEqR);
+				
+				$("#tdMtoInmS").html(d.mtoInmS);
+				$("#tdMtoInmR").html(d.mtoInmR);
+				$("#hMtoInmS").val(d.mtoInmS);
+				$("#hMtoInmR").val(d.mtoInmR);
+				
+				$("#tdMtoVehS").html(d.mtoVehiS);
+				$("#tdMtoVehR").html(d.mtoVehR);
+				$("#hMtoVehS").val(d.mtoVehiS);
+				$("#hMtoVehR").val(d.mtoVehR);
+				
+				$("#tdTranspS").html(d.transpS);
+				$("#tdTranspR").html(d.transpR);
+				$("#hTranspS").val(d.transpS);
+				$("#hTranspR").val(d.transpR);
+				
+				$("#tdLimpS").html(d.limpS);
+				$("#tdLimpR").html(d.limpR);
+				$("#hLimpS").val(d.limpS);
+				$("#hLimpR").val(d.limpR);
+				
+				$("#tdSegS").html(d.segS);
+				$("#tdSegR").html(d.segR);
+				$("#hSegS").val(d.segS);
+				$("#hSegR").val(d.segR);
+				
+				$("#tdTotalR").html(d.totalR);
+				$("#tdTotalS").html(d.totalS);
+				$("#hTotalR").val(d.totalR);
+				$("#hTotalS").val(d.totalS);
+				
+				$(".oculto").slideDown();
 			}
-		}
+		);
 	});
 }
 
--- sus_F/js/reporte.js
+++ sus_F/js/reporte.js
@@ -1,8 +1,22 @@
 function validar() {
+jQuery.validator.addMethod("fechaValida", function(value, element, param) {
+	var anio = $(param).val();
+	var fecha = new Date();
+	var mesActual = fecha.getMonth()+1;
+	var anioActual = fecha.getFullYear();
+	
+	if ( anio <= anioActual ) {
+		return this.optional(element) || value <= mesActual;
+	} else {
+		return this.optional(element);
+	}
+}, "Verifica que el mes seleccionado sea correcto");
+
 	$("#formReporte").validate({
 		rules: {
 			mes: {
-				required: true
+				required: true,
+				fechaValida: "#anio"
 			},
 			anio: {
 				required: true
