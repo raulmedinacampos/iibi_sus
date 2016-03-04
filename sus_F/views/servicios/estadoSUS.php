4ca13d1fbf63a4fdf69b1b75e019c4f9547300cc
Raúl Medina <raulmedinacampos@yahoo.com.mx>
2016-03-04 17:50:08 -0600

    Tabla limpia para cargar datos desde controlador

--- sus_F/views/servicios/estadoSUS.php
+++ sus_F/views/servicios/estadoSUS.php
@@ -56,37 +56,8 @@
 	</div>
 </div>
 
-<?php
-if ( $row = mysqli_fetch_array($seleccion[1]) ) {
-?>
-<table class="table table-condensed table-striped">
-	<tr>
-		<th align="center">Folio</th>
-		<th align="center">Fecha</th>
-		<th align="center">Tipo</th>
-		<th align="center">Estado</th>
-		<th align="center">Acción</th>
-		<th align="center">Respaldo</th>
-	</tr>
-    <?php
-    foreach ( $datos as $dato ) {
-		$idUSolicitante = $dato['idUSolicitante'];
-	?>
-    <tr>
-		<td><a href="#" data-folio="<?=$dato['folio']?>"><?=$dato['folio']?></a></td>
-		<td><?=$dato['fecha']?></td>
-        <td><?= $dato['servicio']?></td>
-        <td><?=$dato['estatus']?></td>
-		<td align="center"><?=$dato['acciones']?></td>
-		<td align="center"><button class="btn btn-sm btn-info btn-pdf" data-folio="<?=$dato['folio']?>">PDF</button></td>
-	</tr>
-    <?php
-	}
-	?>
+<table id="resultados" class="table table-condensed table-striped">
 </table>
-<?php
-}
-?>
 
 <form method="post" id="formPDF" name="formPDF" class="form-horizontal" action="sus-pdf" target="_blank">
 	<input type="hidden" id="hNuevaSolicitud" name="hNuevaSolicitud" value="" />
