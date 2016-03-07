81915e43d4d3509d29123db44eed20e580303eaa
DafneBar <dafne.abad@gmail.com>
2016-03-07 01:26:30 -0600

    para Raul con <3

--- sus_F/controllers/administracion/firmas_autorizadas_controller.php
+++ sus_F/controllers/administracion/firmas_autorizadas_controller.php
@@ -1,22 +1,22 @@
 <?php
 session_start();
 
-$condicion ="empleado.idEmpleado = puesto.idEmpleado, puesto.estatus = 1 and idArea=5 and puesto='Secreatario'";
+$condicion ="empleado.idEmpleado = puesto.idEmpleado and puesto.estatus = 1 and idArea=5 and puesto='Secreatario'";
 $secretario = seleccionar("*", "empleado,puesto", $condicion);
 
-$condicion ="empleado.idEmpleado = puesto.idEmpleado, puesto.estatus = 1 and idArea=5 and puesto='Secreatario'";
+$condicion ="empleado.idEmpleado = puesto.idEmpleado and puesto.estatus = 1 and idArea=5 and puesto='Secreatario'";
 $secretario = seleccionar("*", "empleado,puesto", $condicion);
 
-$condicion ="empleado.idEmpleado = puesto.idEmpleado, puesto.estatus = 1 and idArea=9 and puesto='Jefe de área'";
+$condicion ="empleado.idEmpleado = puesto.idEmpleado and puesto.estatus = 1 and idArea=9 and puesto='Jefe de área'";
 $sGenerales = seleccionar("*", "empleado,puesto", $condicion);
 
-$condicion ="empleado.idEmpleado = puesto.idEmpleado, puesto.estatus = 1 and idArea=6 and puesto='Jefe de departamento'";
+$condicion ="empleado.idEmpleado = puesto.idEmpleado and puesto.estatus = 1 and idArea=6 and puesto='Jefe de departamento'";
 $personal = seleccionar("*", "empleado,puesto", $condicion);
 
-$condicion ="empleado.idEmpleado = puesto.idEmpleado, puesto.estatus = 1 and idArea=8 and puesto='Jefe de departamento'";
+$condicion ="empleado.idEmpleado = puesto.idEmpleado and puesto.estatus = 1 and idArea=8 and puesto='Jefe de departamento'";
 $presupuesto = seleccionar("*", "empleado,puesto", $condicion);
 
-$condicion ="empleado.idEmpleado = puesto.idEmpleado, puesto.estatus = 1 and idArea=7 and puesto='Jefe de departamento'";
+$condicion ="empleado.idEmpleado = puesto.idEmpleado and puesto.estatus = 1 and idArea=7 and puesto='Jefe de departamento'";
 $bienes = seleccionar("*", "empleado,puesto", $condicion);
 
 $data = array(
