a7ebd3ad24ac4fe54adbc27e7fcc988848e13e96
DafneBar <dafne.abad@gmail.com>
2016-03-07 19:57:12 -0600

    herramientas.inc added para funcion contraseña

--- sus_F/controllers/administracion/guardaUsuario_controller.php
+++ sus_F/controllers/administracion/guardaUsuario_controller.php
@@ -1,19 +1,14 @@
 <?php
+require 'inc/herramientas.inc.php';
 
 $maxID=maximo("idUsuario", "usuarioSUS")+1;
 $tipoUsuario = (isset($_POST['grupo'])) ? addslashes($_POST['grupo']) : "";
 $idEmpleado = (isset($_POST['hdn_id'])) ? addslashes($_POST['hdn_id']) : "";
 $usuario = (isset($_POST['usuario'])) ? addslashes($_POST['usuario']) : "";
 $idUsuAutoriza = (isset($_POST['autoriza'])) ? addslashes($_POST['autoriza']) : "";
-$contrasenia = "contra";
+$contrasenia = generarClave();
 
-//$contrasenia = generarClave();
-//$fechaModif = date('Y-m-d H:i:s');
-
-$valores =	$idEmpleado.','.
-			$tipoUsuario.',"'.
-			$usuario.'","'.
-			$contrasenia.'",1,0,now(),NULL,1';
+$valores =	$maxID.','.$idEmpleado.','.$tipoUsuario.',"'.$usuario.'","'.$contrasenia.'",NULL,0,now(),NULL,now(),1';
 
 $insertar = insertar("usuarioSUS", $valores);
 
