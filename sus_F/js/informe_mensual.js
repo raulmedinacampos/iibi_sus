746d47af928f4f57a0b99da78f97dc98bc90e2dc
Raúl Medina <raulmedinacampos@yahoo.com.mx>
2016-03-15 23:16:34 -0600

    Envío mes y año informe nuevo

--- sus_F/js/informe_mensual.js
+++ sus_F/js/informe_mensual.js
@@ -38,76 +38,80 @@ function consultarInforme() {
 		e.preventDefault();
 		
 		if ( $("#formInforme").valid() ) {
-			$("#formInforme").attr("action", "reportes/informe-mensual-pdf");
-			$("#formInforme").attr("target", "_blank");
-			$("#formInforme").submit();
+			$.post(
+				'reportes/infoMes-consultar',
+				{'mes': $("#mes").val(), 'anio': $("#anio").val()},
+				function(data) {
+					if ( data == "1" ) {
+						// Se encontró registro
+						$("#formInforme").attr("action", "reportes/informe-mensual-pdf");
+						$("#formInforme").attr("target", "_blank");
+						$("#formInforme").submit();
+					} else {
+						// No hay registro, se muestra formulario
+						$.post(
+							'reportes/cargar-informe-mensual',
+							{'mes': $("#mes").val(), 'anio': $("#anio").val()},
+							function(data) {
+								var d = jQuery.parseJSON(data);
+								
+								$("#tdCorrespS").html(d.correspS);
+								$("#tdCorrespR").html(d.correspR);
+								$("#hCorrespS").val(d.correspS);
+								$("#hCorrespR").val(d.correspR);
+								
+								$("#tdFotocS").html(d.fotocS);
+								$("#tdFotocR").html(d.fotocR);
+								$("#hFotocS").val(d.fotocS);
+								$("#hFotocR").val(d.fotocR);
+								
+								$("#tdEngarS").html(d.engarS);
+								$("#tdEngarR").html(d.engarR);
+								$("#hEngarS").val(d.engarS);
+								$("#hEngarR").val(d.engarR);
+								
+								$("#tdMtoEqS").html(d.mtoEqS);
+								$("#tdMtoEqR").html(d.mtoEqR);
+								$("#hMtoEqS").val(d.mtoEqS);
+								$("#hMtoEqR").val(d.mtoEqR);
+								
+								$("#tdMtoInmS").html(d.mtoInmS);
+								$("#tdMtoInmR").html(d.mtoInmR);
+								$("#hMtoInmS").val(d.mtoInmS);
+								$("#hMtoInmR").val(d.mtoInmR);
+								
+								$("#tdMtoVehS").html(d.mtoVehiS);
+								$("#tdMtoVehR").html(d.mtoVehR);
+								$("#hMtoVehS").val(d.mtoVehiS);
+								$("#hMtoVehR").val(d.mtoVehR);
+								
+								$("#tdTranspS").html(d.transpS);
+								$("#tdTranspR").html(d.transpR);
+								$("#hTranspS").val(d.transpS);
+								$("#hTranspR").val(d.transpR);
+								
+								$("#tdLimpS").html(d.limpS);
+								$("#tdLimpR").html(d.limpR);
+								$("#hLimpS").val(d.limpS);
+								$("#hLimpR").val(d.limpR);
+								
+								$("#tdSegS").html(d.segS);
+								$("#tdSegR").html(d.segR);
+								$("#hSegS").val(d.segS);
+								$("#hSegR").val(d.segR);
+								
+								$("#tdTotalR").html(d.totalR);
+								$("#tdTotalS").html(d.totalS);
+								$("#hTotalR").val(d.totalR);
+								$("#hTotalS").val(d.totalS);
+								
+								$(".oculto").slideDown();
+							}
+						);
+					}
+				}
+			);
 		}
-	});
-}
-
-function cargarInfo() {
-	$("#btnCargar").click(function(e) {
-		e.preventDefault();
-		
-		$.post(
-			'reportes/cargar-informe-mensual',
-			{},
-			function(data) {
-				var d = jQuery.parseJSON(data);
-				
-				$("#tdCorrespS").html(d.correspS);
-				$("#tdCorrespR").html(d.correspR);
-				$("#hCorrespS").val(d.correspS);
-				$("#hCorrespR").val(d.correspR);
-				
-				$("#tdFotocS").html(d.fotocS);
-				$("#tdFotocR").html(d.fotocR);
-				$("#hFotocS").val(d.fotocS);
-				$("#hFotocR").val(d.fotocR);
-				
-				$("#tdEngarS").html(d.engarS);
-				$("#tdEngarR").html(d.engarR);
-				$("#hEngarS").val(d.engarS);
-				$("#hEngarR").val(d.engarR);
-				
-				$("#tdMtoEqS").html(d.mtoEqS);
-				$("#tdMtoEqR").html(d.mtoEqR);
-				$("#hMtoEqS").val(d.mtoEqS);
-				$("#hMtoEqR").val(d.mtoEqR);
-				
-				$("#tdMtoInmS").html(d.mtoInmS);
-				$("#tdMtoInmR").html(d.mtoInmR);
-				$("#hMtoInmS").val(d.mtoInmS);
-				$("#hMtoInmR").val(d.mtoInmR);
-				
-				$("#tdMtoVehS").html(d.mtoVehiS);
-				$("#tdMtoVehR").html(d.mtoVehR);
-				$("#hMtoVehS").val(d.mtoVehiS);
-				$("#hMtoVehR").val(d.mtoVehR);
-				
-				$("#tdTranspS").html(d.transpS);
-				$("#tdTranspR").html(d.transpR);
-				$("#hTranspS").val(d.transpS);
-				$("#hTranspR").val(d.transpR);
-				
-				$("#tdLimpS").html(d.limpS);
-				$("#tdLimpR").html(d.limpR);
-				$("#hLimpS").val(d.limpS);
-				$("#hLimpR").val(d.limpR);
-				
-				$("#tdSegS").html(d.segS);
-				$("#tdSegR").html(d.segR);
-				$("#hSegS").val(d.segS);
-				$("#hSegR").val(d.segR);
-				
-				$("#tdTotalR").html(d.totalR);
-				$("#tdTotalS").html(d.totalS);
-				$("#hTotalR").val(d.totalR);
-				$("#hTotalS").val(d.totalS);
-				
-				$(".oculto").slideDown();
-			}
-		);
 	});
 }
 
