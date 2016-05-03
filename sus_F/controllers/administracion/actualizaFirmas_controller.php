c0e40071822937b94a610854eacf89f20b09bea9
DafneBar <dafne.abad@gmail.com>
2016-05-03 18:23:21 -0500

    firmas con mensaje de error

--- sus_F/controllers/administracion/actualizaFirmas_controller.php
+++ sus_F/controllers/administracion/actualizaFirmas_controller.php
@@ -6,30 +6,34 @@ $id_sg_ant = (isset($_POST['id_sg_ant'])) ? addslashes($_POST['id_sg_ant']) : ""
 $id_sg = (isset($_POST['hdn_id_sg'])) ? addslashes($_POST['hdn_id_sg']) : "";
 $id_srio = (isset($_POST['hdn_id_srio'])) ? addslashes($_POST['hdn_id_srio']) : "";
 
+$hacerTrans=0;
 
-if($id_srio_ant!=''){
+if($id_srio!=''&&$$id_srio_ant!=''){
 	$anterior = seleccionar('*', 'puesto', 'idPuesto='.$id_srio_ant);
 	$idEmpleado = $id_srio;
 	$idPuestoAnt = $id_srio_ant;
+	$hacerTrans=1;
 }
-
-if($id_sg_ant!=''){
+elseif($id_sg!=''&&$id_sg_ant!=''){
 	$anterior = seleccionar('*', 'puesto', 'idPuesto='.$id_sg_ant);
 	$idEmpleado = $id_sg;
 	$idPuestoAnt= $id_sg_ant;
+	$hacerTrans=1;
 }
 
-$update= "fechaFin=now() and estatus=0 and fechaModif=now()";
- 	
-$maxID=maximo("idPuesto", "puesto")+1;
-$insert = $maxID.",'".$anterior['puesto']."',".$idEmpleado.",".$anterior['idArea'].",".$anterior['correoPuesto'].",now(),now(),1";
-
-$trans = trNuevoSAC($update,$insert,$idPuestoAnt);
-
-if ( $trans[0] == 1 )
-	echo "Se actualizó el nombre de la persona satisfactoriamente.";
-
+if($hacerTrans==1){	
+	$update= "fechaFin=now() and estatus=0 and fechaModif=now()";
+	
+	$maxID=maximo("idPuesto", "puesto")+1;
+	$insert = $maxID.",'".$anterior['puesto']."',".$idEmpleado.",".$anterior['idArea'].",".$anterior['correoPuesto'].",now(),NULL,now(),1";
+	
+	$trans = trNuevoSAC($update,$insert,$idPuestoAnt);
+	
+	if ( $trans[0] == 1 )
+		echo "Se actualizó el nombre de la persona satisfactoriamente.";
+	
+		else
+			echo "Ocurió un problema, favor de comunicarse con el adminsitrador.";}
 else
-	echo "Ocurió un problema, favor de comunicarse con el adminsitrador.";
-
+	{echo "[TR] Ocurió un problema, favor de comunicarse con el adminsitrador.";} 
 ?>
