4b2b54d455a4287c79634d83421a4283180a287c
Raúl Medina <raulmedinacampos@yahoo.com.mx>
2016-05-03 16:37:58 -0500

    Actualización firmas autorizadas

--- sus_F/js/firmas_autorizadas.js
+++ sus_F/js/firmas_autorizadas.js
@@ -1,5 +1,5 @@
 function autocompletar() {
-	$('.typeahead').typeahead({
+	$('#administrativo').typeahead({
 		displayKey: 'nombre',
 		source: function(query, process) {
 		    map = {};
@@ -18,23 +18,85 @@ function autocompletar() {
 			);
 		},
 		updater: function(item) {
-	        $("#hdn_id").val(map[item].id);
+	        $("#hdn_id_strio").val(map[item].id);
 	        return item;
 	    }
-	}); 
+	});
+	
+	$('#serviciosGenerales').typeahead({
+		displayKey: 'nombre',
+		source: function(query, process) {
+		    map = {};
+			$.getJSON('listado-firmas',
+					{'q': query},
+					function(data) {
+						objects = [];
+						
+						$.each(data, function(i, object) {
+							map[object.nombre] = object;
+							objects.push(object.nombre);
+				        });
+						
+						process(objects);
+					}
+			);
+		},
+		updater: function(item) {
+	        $("#hdn_id_sg").val(map[item].id);
+	        return item;
+	    }
+	});
 }
 
 function actualizar() {
 	$("#btnAdministrativo").click(function(e) {
 		e.preventDefault();
 		
-		var nuevo = $("#administrativo").val();
+		var anterior = $("#id_strio_ant").val();
+		var nuevo = $("#hdn_id_strio").val();
+		
+		$.post(
+			'administracion/actualiza-firmas', 
+			{'id_srio_ant':anterior, 'hdn_id_srio':nuevo}, 
+			function(data) {
+				$(".modal-header .modal-title").html("Firmas autorizadas");
+				$(".modal-footer .btn-default").html("Cerrar");
+				$(".modal-footer .btn-primary").css("display", "none");
+				$(".modal-body").html(data);
+				$("#myModal").modal('show');
+				
+				$("#myModal .modal-footer .btn-default").click(function() {
+					$("#myModal").modal('hide');
+						
+					$("#miDiv").load("administracion/firmas-autorizadas");
+				});
+			}
+		);
 	});
 	
 	$("#btnServiciosGenerales").click(function(e) {
 		e.preventDefault();
 		
-		var nuevo = $("#serviciosGenerales").val();
+		var anterior = $("#serviciosGenerales").val();
+		var nuevo = $("#hdn_id_sg").val();
+		
+		$.post(
+			'administracion/actualiza-firmas', 
+			{'id_sg_ant':anterior, 'hdn_id_sg':nuevo}, 
+			function(data) {
+				$(".modal-header .modal-title").html("Firmas autorizadas");
+				$(".modal-footer .btn-default").html("Cerrar");
+				$(".modal-footer .btn-primary").css("display", "none");
+				$(".modal-body").html(data);
+				$("#myModal").modal('show');
+				
+				$("#myModal .modal-footer .btn-default").click(function() {
+					$("#myModal").modal('hide');
+						
+					$("#miDiv").load("administracion/firmas-autorizadas");
+				});
+			}
+		);
 	});
 	
 	$("#btnPersonal").click(function(e) {
